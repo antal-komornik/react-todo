eaa2979086ed7292d63436dff8e63f8ac2f98950
Komornik Antal <55227338+antal-komornik@users.noreply.github.com>
2023-04-28 14:41:14 +0000

    Update IaesdnputForm.js

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,19 +1,36 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 const InputForm = () => {
+  const [task, setTask] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const t = event.target.taskField.value;
+    setTask( task=t );
+
+    // event.target.reset();
+    // console.log(task);
+  };
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3">
           <Form.Label>Add New Task</Form.Label>
-          <Form.Control type="task" placeholder="Enter new task" />
+          <Form.Control
+            id="taskField"
+            type="task"
+            placeholder="Enter new task"
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
           ADD
         </Button>
       </Form>
+
+      <h1>{task} </h1>
     </div>
   );
 };
